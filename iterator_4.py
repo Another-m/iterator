dd32d1858539b5f5150951df5ba2196becd0388b
Another-m <88193511+Another-m@users.noreply.github.com>
2022-04-11 19:23:35 +0300

    Update iterator_4.py

--- iterator_4.py
+++ iterator_4.py
@@ -1,13 +1,13 @@
 def flat_generator(nested_list):
-	new_list = []
 	for i in nested_list:
 		if type(i) == list:
-			value = flat_generator(i)
-			new_list += value
+			list_value = list(flat_generator(i))
+			for value in list_value:
+				yield value
 		else:
 			value = i
-			new_list += [value]
-	return new_list
+			yield value
+
 
 nested_list = [
 	['a', 'b', 'c'],
@@ -16,7 +16,7 @@ nested_list = [
 ]
 
 
-for item in  flat_generator(nested_list):
+for item in flat_generator(nested_list):
 	print(item)
 
 flat_list = [item for item in flat_generator(nested_list)]
